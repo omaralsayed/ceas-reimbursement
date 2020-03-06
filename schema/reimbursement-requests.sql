4033d796a0a227c1c6e916a7d113885abf9bf281
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-03-05 19:25:56 -0500

    Update schema

---
+++ schema/reimbursement-requests.sql
@@ -0,0 +1,39 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `reimbursement-requests` (
+  `uid` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `position` text NOT NULL,
+  `email` text NOT NULL,
+  `mid` text NOT NULL,
+  `date` date NOT NULL,
+  `vendor` text NOT NULL,
+  `amount` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `approved` text NOT NULL,
+  `payment` text NOT NULL,
+  `receipt` text NOT NULL,
+  `documents` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `reimbursement-requests`
+  ADD PRIMARY KEY (`uid`);
+
+
+ALTER TABLE `reimbursement-requests`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
