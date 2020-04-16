8da5e201e2132ca09279125de5388a9891ddc1db
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-04-16 19:26:08 -0400

    Update database schema

--- schema/reimbursement_admin.sql
+++ schema/reimbursement_admin.sql
@@ -1,5 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Server version: 5.7.24-log
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tribunal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reimbursement_admin`
+--
+
 CREATE TABLE `reimbursement_admin` (
   `admin_name` text NOT NULL,
   `admin_email` text NOT NULL,
   `super_email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reimbursement_admin`
+--
+
+INSERT INTO `reimbursement_admin` (`admin_name`, `admin_email`, `super_email`) VALUES
+('Admin', 'admin@mail.uc.edu', 'super@mail.uc.edu');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
