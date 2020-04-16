da6d3babcf984a83eb43518e3f041ccd73a2df0b
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-04-16 18:27:32 -0400

    Update SQL query

--- api/get_info.php
+++ api/get_info.php
@@ -4,24 +4,11 @@ error_reporting(-1);
 set_include_path('./includes/');
 require_once('mysqli.php');
 
-// Initialize SQL result storage
-if (!isset($result_data))
-    $result_data = new stdClass();
-$result_data->status = 'error';
-$result_data->data[] = array();
-
-$sql = 'SELECT admin_name FROM reimbursement_requests';
+$sql = 'SELECT `admin_email` FROM `reimbursement_admin`';
 
 $result = $mysqli->query($sql);
 
-if ($result) {
-    while ($row = $result->fetch_assoc()) {
-        $result_data->data[] = $row;
-    }
-    $result_data->status = 'success';
-}
-
-echo json_encode($result_data);
+if ($result) echo json_encode($result->fetch_assoc()['admin_email']);
 
 mysqli_close($mysqli);
 ?>
